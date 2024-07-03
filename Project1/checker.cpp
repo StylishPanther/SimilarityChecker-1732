660110186672347a8cf5db5f3c83429c8264ed72
jinho <zazz0907@naver.com>
2024-07-03 17:08:31 +0900

    Length feature TDD cycle green is done.

--- Project1/checker.cpp
+++ Project1/checker.cpp
@@ -1,2 +1,19 @@
 #include <string>
 using namespace std;
+
+class SimilarityChecker
+{
+public:
+	SimilarityChecker(string str1, string str2) : first{ first }, second{ second }
+	{
+
+	}
+	int lengthChecker(void)
+	{
+		if (first.length() == second.length())
+			return 60;
+	}
+private:
+	string first;
+	string second;
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,3 +2,15 @@
 #include "../Project1/checker.cpp"
 #include <string>
 using namespace std;
+
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
+	SimilarityChecker check{"sd", "ds"};
+
+};
+
+TEST_F(SimilarityCheckerFixture, length_check_test_1)
+{
+	EXPECT_EQ(60, check.lengthChecker());
+}
